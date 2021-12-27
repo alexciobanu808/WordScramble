9c6c8cb981b4c6c141e8229e96e17be4e643f81f
Bogdan Alex Ciobanu <82522105+alexciobanu808@users.noreply.github.com>
2021-12-27 13:14:37 -0500

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Alex Ciobanu on 12/27/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
