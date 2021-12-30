2ca6ef9b836e887d46476210e8a4175ab91e37a3
Bogdan Alex Ciobanu <82522105+alexciobanu808@users.noreply.github.com>
2021-12-30 17:26:02 -0500

    Added data validation and adjusted focus state

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -11,6 +11,8 @@ struct ContentView: View {
     @State private var usedWords = [String]()
     @State private var rootWord = ""
     @State private var newWord = ""
+    @State private var score = 0
+    @FocusState private var isNewWordFocused: Bool
     
     @State private var errorTitle = ""
     @State private var errorMessage = ""
@@ -19,12 +21,17 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             List {
-                Section {
+                Section() {
                     TextField("Enter your word", text: $newWord)
                         .autocapitalization(.none)
+                        .focused($isNewWordFocused)
                 }
                 
-                Section {
+                Section("Score") {
+                    Text("\(score)")
+                }
+                
+                Section(usedWords.count == 0 ? "" : "Guessed words"){
                     ForEach(usedWords, id: \.self) { word in
                         HStack {
                             Image(systemName: "\(word.count).circle")
@@ -41,12 +48,25 @@ struct ContentView: View {
             } message: {
                 Text(errorMessage)
             }
+            .toolbar {
+                Button("Restart") {
+                    startGame()
+                }
+            }
         }
     }
     
     func addNewWord() {
         let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
-        guard answer.count > 0 else { return }
+        guard answer.count > 2 else {
+            wordError(title: "Word length too short", message: "Word must be longer than 2 characters!")
+            return
+        }
+        
+        guard answer == newWord else {
+            wordError(title: "Word same as start word", message: "Cannot use same word as start word!")
+            return
+        }
         
         guard isOriginal(word: answer) else {
             wordError(title: "Word used already", message: "Be more original!")
@@ -63,10 +83,15 @@ struct ContentView: View {
             return
         }
         
+        
+        
         withAnimation {
             usedWords.insert(answer, at: 0)
         }
+        
         newWord = ""
+        keepScore(word: answer)
+        isNewWordFocused = true
     }
     
     func startGame() {
@@ -74,11 +99,19 @@ struct ContentView: View {
             if let startWords = try? String(contentsOf: startWordsURL) {
                 let allWords = startWords.components(separatedBy: "\n")
                 rootWord = allWords.randomElement() ?? "silkworm"
+                usedWords = [String]()
+                newWord = ""
+                score = 0
+                isNewWordFocused = true
                 return
             }
         }
         
         fatalError("Could not load start.txt from bundle.")
+    }
+    
+    func keepScore(word: String) {
+        score += word.count
     }
     
     func isOriginal(word: String) -> Bool {
@@ -111,6 +144,8 @@ struct ContentView: View {
         errorTitle = title
         errorMessage = message
         showingError = true
+        newWord = ""
+        isNewWordFocused = true
     }
 }
 
